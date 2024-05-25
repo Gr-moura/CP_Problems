c56d454199656b469af71f37f56674a6de4ee6b5
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-05-24 22:54:42 -0300

    Remaking an old one

--- First_Semester/Solves/BC_3420_HouseofCards.cpp
+++ First_Semester/Solves/BC_3420_HouseofCards.cpp
@@ -5,7 +5,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define _ ios_base ::sync_with_stdio(0);cin.tie(0);
+#define _                                                                                                              \
+    ios_base ::sync_with_stdio(0);                                                                                     \
+    cin.tie(0);
+
 #define int long long
 #define vi vector<int>
 #define endl '\n'
@@ -18,36 +21,46 @@ using namespace std;
 #define char unsigned char
 
 typedef long double ld;
+int formula(int i)
+{
+    int resposta;
+    i = i - 1;
+    resposta = (3 * (i * i) + 7 * i + 4) / 2;
+    return resposta;
+}
 
 int32_t main()
-{ _ 
-    vector<pair<int, int>> v;
-    int t = 1;
+{
+    _ int t = 1;
     cin >> t;
-    vector <int> pedidos(t); for (auto& i : pedidos) cin >> i;
-    
-    int cartas = 2;
-    v.push_back({2, 1});
-    for (int i = 1; cartas <= 1000000000000000000; i++)
-    {
-        cartas = v[i - 1].f + i + (i + 1) * 2;
-        v.pb(cartas);
-    }
-    
 
-    for (int i = 1; i <= t; i++)
+    while (t--)
     {
-        int c;
-        cin >> c;
-        int j;
-        for (j = 0; v[j] <= c; j++)
-            ;
-        cout << j << endl;
-    }
+        int buscar;
+        cin >> buscar;
+        int inicio = 1, fim = 9e8;
 
-#ifdef ONPC
-    cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
-#endif
+        while (!(formula((inicio + fim) / 2) <= buscar && formula((inicio + fim) / 2 + 1) >= buscar))
+        {
+            int buscamos = formula((inicio + fim) / 2);
+            if (buscamos == buscar || formula((inicio + fim) / 2 + 1) == buscar)
+            {
+            }
+            else if (buscamos > buscar)
+                fim = (inicio + fim) / 2;
+            else if (buscamos < buscar)
+                inicio = (inicio + fim) / 2;
+            if (buscamos == buscar || formula((inicio + fim) / 2 + 1) == buscar)
+            {
+                if (formula((inicio + fim) / 2 + 1) == buscar)
+                    fim += 2;
+                break;
+            }
+
+            // if (buscamos <= buscar && formula((inicio + fim) / 2 + 1) >= buscar)
+        }
+        cout << (inicio + fim) / 2 << endl;
+    }
 
     return 0;
 }
