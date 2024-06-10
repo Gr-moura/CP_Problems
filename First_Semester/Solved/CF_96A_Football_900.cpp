34a38a734d3ba1907195672c591e4dbbef5a90f4
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-06-09 21:17:04 -0300

    +1 solve 09/06

---
+++ First_Semester/Solved/CF_96A_Football_900.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io cin.tie(0)->sync_with_stdio(0);
+#define dbg(x) cout << #x << " = " << x << endl
+#define all(x) x.begin(), x.end()
+#define sz(a) ((int)((a).size()))
+#define int long long
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define lb(vect, x) (lower_bound(all(vect), x) - vect.begin())
+#define ub(vect, x) (upper_bound(all(vect), x) - vect.begin())
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    char ultimo = s[0];
+    int buscar = 1;
+
+    for (int i = 1; i < s.size(); i++)
+    {
+
+        if (s[i] == ultimo)
+        {
+            buscar++;
+        }
+        else
+        {
+            ultimo = s[i];
+            buscar = 1;
+        }
+        if (buscar == 7)
+        {
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
+int32_t main()
+{
+    // casas decimais
+    // cout << fixed << setprecision(1);
+
+    // horario
+    // cout << setfill('0') << setw(2);
+
+    fast_io;
+    int t = 1;
+    // cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+
+        // #ifdef ONPC
+        //         cout << "__________________________" << endl;
+        // #endif
+    }
+
+    return 0;
+}
