12e2b9bc599f6b275104e679d15fd3e7410b6970
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-06-11 23:50:27 -0300

    +1 solve 11/06

---
+++ First_Semester/Solved/CF_160A_Twins_900.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io cin.tie(0)->sync_with_stdio(0);
+#define dbg(x) cout << #x << " = " << x << endl
+#define all(x) x.begin(), x.end()
+#define sz(a) ((int)((a).size()))
+#define int long long
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define lb(vect, x) (lower_bound(all(vect), x) - vect.begin())
+#define ub(vect, x) (upper_bound(all(vect), x) - vect.begin())
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve()
+{
+    int sz, soma = 0;
+    cin >> sz;
+
+    vi coins(sz);
+    for (int i = 0; i < sz; i++)
+    {
+        cin >> coins[i];
+        soma += coins[i];
+    }
+
+    sort(all(coins), greater<>());
+
+    int atual = 0, resto = soma;
+    for (int i = 0; i < sz; i++)
+    {
+        atual += coins[i];
+        resto -= coins[i];
+        if (atual > resto)
+        {
+            cout << i + 1;
+            break;
+        }
+    }
+}
+
+int32_t main()
+{
+    // casas decimais
+    // cout << fixed << setprecision(1);
+
+    // horario
+    // cout << setfill('0') << setw(2);
+
+    fast_io;
+    int t = 1;
+    // cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+
+        // #ifdef ONPC
+        //         cout << "__________________________" << endl;
+        // #endif
+    }
+
+    return 0;
+}
