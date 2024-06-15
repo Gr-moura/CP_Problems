87a340d151f7e3759c47db631b8e16440f115f58
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-06-15 19:03:36 -0300

    +1 solve 19/03

---
+++ First_Semester/Solved/Doing/CF_189A_CutRibbon_1300.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io cin.tie(0)->sync_with_stdio(0);
+#define dbg(x) cout << #x << " = " << x << endl
+#define all(x) x.begin(), x.end()
+#define sz(a) ((int)((a).size()))
+#define int long long
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define lb(vect, x) (lower_bound(all(vect), x) - vect.begin())
+#define ub(vect, x) (upper_bound(all(vect), x) - vect.begin())
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(3);
+    for (auto &i : v)
+        cin >> i;
+
+    sort(v.begin(), v.end());
+
+    int temp, a = v[0], b = v[1], c = v[2];
+    int soma = (n / a) * a;
+    int p = n / a;
+    int it = 1;
+    while (soma != n)
+    {
+        soma = (n / a) * a - a * it;
+        p = (n / a) - it;
+
+        if ((n - soma) % b == 0)
+        {
+            p += (n - soma) / b;
+            cout << p;
+            return;
+        }
+
+        else
+        {
+            int nb = (n - soma) / b;
+
+            while (nb >= 0)
+            {
+
+                temp = soma + nb * b;
+                int nc = (n - temp) / c;
+
+                if ((n - temp) % c == 0)
+                {
+                    dbg(nb);
+                    dbg(nc);
+                    dbg(p);
+                    p += nb;
+                    p += nc;
+                    cout << p;
+                    return;
+                }
+
+                nb--;
+            }
+        }
+
+        it++;
+    }
+
+    cout << p;
+}
+
+int32_t main()
+{
+    // casas decimais
+    // cout << fixed << setprecision(1);
+
+    // horario
+    // cout << setfill('0') << setw(2);
+
+    fast_io;
+    int t = 1;
+    // cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+
+        // #ifdef ONPC
+        //         cout << "__________________________" << endl;
+        // #endif
+    }
+
+    return 0;
+}
