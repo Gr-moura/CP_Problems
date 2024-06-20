c5e7f6c9fc5ff3c0c3b19e9238272fda4f447e11
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-06-20 20:57:43 -0300

    +1 solve 20/06

---
+++ First_Semester/Solved/Doing/CF_977A_WrongSubtraction_800.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io cin.tie(0)->sync_with_stdio(0);
+#define dbg(x) cout << #x << " = " << x << endl
+#define all(x) x.begin(), x.end()
+#define sz(a) ((int)((a).size()))
+#define int long long
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define lb(vect, x) (lower_bound(all(vect), x) - vect.begin())
+#define ub(vect, x) (upper_bound(all(vect), x) - vect.begin())
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+void solve()
+{
+    int n, t;
+    cin >> n >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        if (n % 10 != 0)
+            n--;
+        else
+            n /= 10;
+    }
+
+    cout << n;
+}
+
+int32_t main()
+{
+    // casas decimais
+    // cout << fixed << setprecision(1);
+
+    // horario
+    // cout << setfill('0') << setw(2);
+
+    fast_io;
+    int t = 1;
+    // cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+
+        // #ifdef ONPC
+        //         cout << "__________________________" << endl;
+        // #endif
+    }
+
+    return 0;
+}
