c6b5ec99b95e211bc41cbee7c74a3567ad80090e
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-07-29 11:59:46 -0300

    Adicionada explicacao para o 1996E

--- First_Semester/Solved/Doing/CF_1996E_Decode.cpp
+++ First_Semester/Solved/Doing/CF_1996E_Decode.cpp
@@ -20,43 +20,56 @@ typedef vector<int> vi;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
+const int MOD = 1e9 + 7;
+
 void solve()
 {
-    string a;
-    cin >> a;
+    string S;
+    cin >> S;
 
+    int n = S.size();
+    S = " " + S;
+
+    vector<int> Prefixo_Somas(n + 1, 0);
+
+    for (int i = 1; i <= n; i++)
+    {
+        Prefixo_Somas[i] = Prefixo_Somas[i - 1] + (S[i] == '0' ? -1 : 1);
+    }
+
+    map<int, int> cnt;
     int ans = 0;
 
-    int n = a.size();
-
-    vector<pii> pre(n + 1, pii(0, 0));
-
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <= n; i++)
     {
-        pre[i + 1] = pre[i];
+        /*
+         * Para encontrarmos uma resposta, multiplicamos a pos do atual
+         * com a pos de um elemento anterior com a mesma soma e, portanto,
+         * precisamos que a mesma soma apareça no minímo duas vezes.
+         *
+         * Assim, a cnt[Prefixo_Somas[i]] começa em 0 e adicionamos a
+         * posicao do ultimo elemento com aquele valor, pois na primeira vez
+         * sera multiplicada por 0, e na segunda vez que aparecer, ira
+         * ser multiplicada pela posicao do inicial.
+         *
+         * Ja quando sao 3 casos, nós multiplicaríamos a pos atual com
+         * a soma do primeiro com aquele valor com o segundo com aquele valor,
+         * pois a resposta seria
+         *
+         * = atual com 3 e atual com 2
+         * = (n - atual + 1)(pos_primeiro) + (n - atual + 1)(pos_segundo)
+         * = (n - atual + 1)(pos_primeiro + pos_segundo)
+         *
+         * E o caso do primeiro com o segundo ja teria sido encontrado anteriormente
+         */
 
-        if (a[i] == '0')
-            pre[i + 1].f++;
-        else
-            pre[i + 1].s++;
+        ans += (n - i + 1) * cnt[Prefixo_Somas[i]];
+        cnt[Prefixo_Somas[i]] += i + 1;
+
+        cnt[Prefixo_Somas[i]] %= MOD;
+        ans %= MOD;
     }
 
-    for (int i = 0; i <= n - 1; i++)
-    {
-        for (int j = i + 1; j <= n; j++)
-        {
-            int zeros, uns;
-            zeros = pre[j].f - pre[i].f;
-            uns = pre[j].s - pre[i].s;
-
-            if (zeros == uns)
-            {
-                ans += (n - j + 1) * (i + 1);
-            }
-
-            ans %= 1000000007;
-        }
-    }
     cout << ans << endl;
 }
 
